e6f9e266d1d734702dd134288a7bdeedc5b88a42
Serjml <Serjml@list.ru>
2022-01-08 17:30:59 +0300

    feat: solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  n = n.toString();
+  for (i = 0; i < n.length; i++) {
+    if (n[i] === '-') {
+      result += result;
+    } else {
+      result = `${n[i]}${result}`;
+    }          
+  }
+  return +result;  
 }
